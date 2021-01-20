c133a4c6578d7d36236492776e31b2b52d3799a6
RomanChubak <roman.chubak@ebizautos.com>
2021-01-20 18:02:14 +0200

    commit)

--- sycnPhotosConsole/Program.cs
+++ sycnPhotosConsole/Program.cs
@@ -15,6 +15,10 @@ using Newtonsoft.Json;
 // but I rejected this idea
 // because i had not found the library for converting photo files to apple file
 
+
+//it is the console app, but it is very bad do all work in one class
+//it is only first try to download files from google photos
+
 namespace sycnPhotosConsole { //bad namespace name
    class Program {
       static void Main(string[] args) {
@@ -29,6 +33,7 @@ namespace sycnPhotosConsole { //bad namespace name
          string ClientSecret = "***secret***";
 
          UserCredential credential;
+         //need some manager class for this, hardcode is bad
          using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
          {
             credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
@@ -39,8 +44,8 @@ namespace sycnPhotosConsole { //bad namespace name
                 new FileDataStore(credPath, true)).Result; //bad usage of async methods
          }
 
-         try
-         {
+         //need some class for configuring this
+         try {
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://photoslibrary.googleapis.com/v1/mediaItems:search");
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Headers.Add("client_id", ClientID);
@@ -48,6 +53,7 @@ namespace sycnPhotosConsole { //bad namespace name
             httpWebRequest.Headers.Add("Authorization:" + credential.Token.TokenType + " " + credential.Token.AccessToken);
             httpWebRequest.Method = "POST";
 
+            //need some builder to build filter, maybe there are some external libraries for this
             var filters = new Filters() {
                DateFilters = new DateFilters() {
                   Dates = new List<GoogleFilterDate>() {
@@ -60,7 +66,7 @@ namespace sycnPhotosConsole { //bad namespace name
                }
             };
 
-            string jsonFilter = JsonConvert.SerializeObject(new {filters});
+            string jsonFilter = JsonConvert.SerializeObject(new {filters}); // this is bad "new {filters}", the same problem, need builder
             
             using (Stream requestStream = httpWebRequest.GetRequestStream()) {
                ASCIIEncoding encoding = new ASCIIEncoding ();
@@ -72,14 +78,14 @@ namespace sycnPhotosConsole { //bad namespace name
             
 
             HttpWebResponse response = httpWebRequest.GetResponse() as HttpWebResponse;
-            using (Stream responseStream = response.GetResponseStream())
+            using (Stream responseStream = response.GetResponseStream()) //response can be null
             {
                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
 
                СlsResponseRootObject responseObject = new СlsResponseRootObject(); //bad name for response
                responseObject = JsonConvert.DeserializeObject<СlsResponseRootObject>(reader.ReadToEnd());
 
-               if (responseObject != null)
+               if (responseObject != null) // bad because create only one if block like if (responseObject?.mediaItems != null && responseObject.mediaItems.Count > 0)
                {
                   if (responseObject.mediaItems != null && responseObject.mediaItems.Count > 0)
                   {
@@ -97,11 +103,11 @@ namespace sycnPhotosConsole { //bad namespace name
                               throw new Exception("unknown mime type");
                            }
                            
-                           string filesDirectory = $"Photos/{filters.DateFilters.Dates[0].Year}_{filters.DateFilters.Dates[0].Month}_{filters.DateFilters.Dates[0].Day}";
+                           string filesDirectory = $"Photos/{filters.DateFilters.Dates[0].Year}_{filters.DateFilters.Dates[0].Month}_{filters.DateFilters.Dates[0].Day}"; // put this pattern in configuration
                            
-                           Directory.CreateDirectory(filesDirectory);
+                           Directory.CreateDirectory(filesDirectory); // need try catch only for this
                            
-                           client.DownloadFile(new Uri(url), $"{filesDirectory}/{item.filename}");
+                           client.DownloadFile(new Uri(url), $"{filesDirectory}/{item.filename}"); // put this pattern in configuration
                         }
                         Console.WriteLine($"ID:{item.id}, Filename:{item.filename}, MimeType:{item.mimeType}");
                      }
@@ -109,8 +115,7 @@ namespace sycnPhotosConsole { //bad namespace name
                }
             }
          }
-         catch (Exception ex)
-         {
+         catch (Exception ex) {
             Console.WriteLine("Error occured: " + ex.Message);
          }
       } 
